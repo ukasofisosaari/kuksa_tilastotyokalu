336f442a2300a0f19266aab1b08b6c2da56d5560
Saku Rautiainen <saku.rautiainen@iki.fi>
2017-08-08 19:49:06 +0300

    Report template for job analyzer

--- plugins/JonAnalyzer.py
+++ plugins/JonAnalyzer.py
@@ -14,6 +14,17 @@ class TestStatisticsCalculator(StatisticsCalculatorBase):
     def calculate_statistics(self, parameters=[]):
         print("This is a test class")
 
+        #Kuva 1
+        #Henkilö placeholder on <PERSONS>, pitää näyttää tältä: 'Saku', 'Tero', 'Olli'
+        #Alla esimerkki datasetistä mitä pitää insertoida. PLACEHOLDER on <JOB>
+        #{
+		#	label: "Lippukunnanjohtajat",
+        #    data: [1,0,0],
+        #   backgroundColor: "rgba(63,103,126,1)",
+        #    hoverBackgroundColor: "rgba(50,90,100,1)"
+        #},
+
+
 
 def registerCalculatorPlugin():
     return TestStatisticsCalculator
